64181500116fa95a92f0a054480119a2a9badf70
CIbeaST <61186415+CIbeaST@users.noreply.github.com>
2021-11-29 04:22:02 +0300

    Add files via upload

--- RUN_v1.bat
+++ RUN_v1.bat
@@ -1,5 +1,7 @@
 @echo off
 color a
+pip install --upgrade pip
+pip install torch -f https://download.pytorch.org/whl/torch_stable.html
 pip install -q torchaudio omegaconf
 pip install -q logmmse
 python demo_v1.py
